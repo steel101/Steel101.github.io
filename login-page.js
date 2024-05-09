e75ba8afa831746038886e3512d4e1f72d31780f
steel johnson <steelvargas@gmail.com>
2024-05-08 22:45:42 -0400

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -10,7 +10,7 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "svoc" && password === "123") {
+    if (username === "bad" && password === "note") {
         // If the credentials are valid, show an alert box and reload the page
         window.location.href = "https://steel101.github.io/Badday.txt";
     } else {
